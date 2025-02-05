5f8d04be06878209547704f59d235cddd3b704e0
Pedromm20 <pedromulamontesinos@gmail.com>
2025-02-05 20:48:43 +0100

    v7

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
+import netlify from "@astrojs/netlify";
 
 
 // https://astro.build/config
 export default defineConfig({
-    vite: {
-        plugins: [tailwindcss()],
-      },
+  vite: {
+      plugins: [tailwindcss()],
+    },
+  output: 'server',
+  adapter: netlify({
+    edgeMiddleware: true
+  }),
 });
